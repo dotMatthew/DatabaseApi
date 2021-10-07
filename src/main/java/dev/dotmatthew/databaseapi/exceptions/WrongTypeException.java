63852021c3776f8518e34a798a10544c92484f7c
Mathias Dollenbacher <hello@mdollenbacher.net>
2021-10-07 10:19:20 +0200

    #13 Add exception for wrong column type

---
+++ src/main/java/dev/dotmatthew/databaseapi/exceptions/WrongTypeException.java
@@ -0,0 +1,9 @@
+package dev.dotmatthew.databaseapi.exceptions;
+
+public class WrongTypeException extends RuntimeException {
+
+    public WrongTypeException(String message) {
+        super(message);
+    }
+
+}
